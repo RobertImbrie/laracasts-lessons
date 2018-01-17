e6b8b289bd6542d0a524944cc2b5856b08f19df3
RobertImbrie <robert.m.imbrie@gmail.com>
2018-01-17 13:39:25 -0600

    Use query scopes

--- blog/app/Task.php
+++ blog/app/Task.php
@@ -6,8 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    public static function incomplete()
+    public static function scopeIncomplete($query)
     {
-        return static::where('completed', 0)->get();
+        return $query->where('completed', 0);
     }
 }
